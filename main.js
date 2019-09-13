12b32c05f057d15272a6590a84a92af3a1d6b1f7
ekegus <benjamin.ekeloef@gmail.com>
2019-09-13 18:39:43 +0100

    added function to calculate highest category score

--- main.js
+++ main.js
@@ -4,17 +4,34 @@ const plantData = require("./data.json");
 const calcSumOfCategoryLabels = (imageString, plantCategories) => {
   const plant = plantData[imageString];
   let categoryScores = new Object();
-  for (let [plantCategory, plantLabel] of Object.entries(plantCategories)) {
+  for (let [plantCategory, categoryLabel] of Object.entries(plantCategories)) {
     plant.forEach(dataLabel => {
-      if (plantLabel.indexOf(dataLabel.description) !== -1) {
-        categoryScores[plantCategory] = dataLabel.score;
-        // categoryScores.push(dataLabel.description);
-        console.log(plantCategory);
-        console.log(`Your image belongs in the ${plantCategory} category.`);
+      if (categoryLabel.indexOf(dataLabel.description) !== -1) {
+        categoryScores.hasOwnProperty(plantCategory)
+          ? (categoryScores[plantCategory] += dataLabel.score)
+          : (categoryScores[plantCategory] = dataLabel.score);
       }
     });
   }
   console.log(categoryScores);
+
+  let highestCategory = getCategoryWithHighestScore(categoryScores);
+  console.log(highestCategory);
+  console.log(categoryScores[highestCategory]);
+  console.log(
+    `Your plant image belongs to the ${highestCategory} with a score of ${categoryScores[highestCategory]}`
+  );
+  // categoryScores[highestCategory]
 };
 
-calcSumOfCategoryLabels("19.jpg", plantCategories);
+const getCategoryWithHighestScore = objectOfCategoryScores => {
+  let category = Object.keys(objectOfCategoryScores).filter(category => {
+    return (
+      objectOfCategoryScores[category] ===
+      Math.max.apply(null, Object.values(objectOfCategoryScores))
+    );
+  });
+  return category[0];
+};
+
+calcSumOfCategoryLabels("5.jpg", plantCategories);
